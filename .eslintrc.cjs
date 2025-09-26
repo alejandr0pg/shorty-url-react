cab4013805d3c49f5aeefa3ccbf7222013fcc03c
Alejandro Pérez <alejandroprz2011@gmail.com>
2025-09-26 01:50:36 -0400

    Actualizar configuración de ESLint para usar 'plugin:@typescript-eslint/recommended' y agregar 'node_modules' a los patrones de ignorar.

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -8,11 +8,11 @@ module.exports = {
   },
   extends: [
     'eslint:recommended',
-    '@typescript-eslint/recommended',
+    'plugin:@typescript-eslint/recommended',
     'plugin:react/recommended',
     'plugin:react-hooks/recommended'
   ],
-  ignorePatterns: ['dist', '.eslintrc.js'],
+  ignorePatterns: ['dist', '.eslintrc.cjs', 'node_modules'],
   parser: '@typescript-eslint/parser',
   parserOptions: {
     ecmaVersion: 'latest',
